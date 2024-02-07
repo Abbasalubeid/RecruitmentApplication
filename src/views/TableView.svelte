17f75949ae1e6ac5ed00de2a5782cd4ff5e082e9
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-08 00:53:18 +0100

    TableView usin skeleton

---
+++ src/views/TableView.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { Table } from '@skeletonlabs/skeleton';
+  import type { TableSource } from '@skeletonlabs/skeleton';
+
+  export let head: string[] = [];
+  export let body: any[][] = [];
+
+  let tableSource: TableSource = { head, body};
+</script>
+
+<Table source={tableSource} />
