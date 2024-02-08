19cad9c4e1e558489d78235c0c4fa6a5339167e4
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-08 16:39:27 +0100

    Prettier formatting

--- src/routes/competences/+page.svelte
+++ src/routes/competences/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import CompetencePresenter from '../../presenters/CompetencePresenter.svelte';
-  </script>
-  
-  <CompetencePresenter/>
-  
+</script>
+
+<CompetencePresenter />
