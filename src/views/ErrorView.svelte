28d5f903238e551a31c35de24aba557b9762dcf7
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-08 13:26:59 +0100

    Error view

---
+++ src/views/ErrorView.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let message: string;
+</script>
+
+{#if message}
+	<aside class="alert variant-ghost border border-red-400 bg-red-100 text-red-700">
+		<div class="alert-message">
+			<h3 class="text-lg font-semibold">Error</h3>
+			<p>{message}</p>
+		</div>
+	</aside>
+{/if}
