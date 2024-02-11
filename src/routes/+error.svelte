61cb77e5451195e49bc32489f90a524ee4daffeb
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-11 18:47:20 +0100

    +error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import ErrorView from '../views/ErrorView.svelte';
+
+	let errorMessage = $page.error?.message || 'An unexpected error occurred';
+	let errorStatus = $page.status || 500;
+</script>
+
+<ErrorView {errorMessage} {errorStatus}></ErrorView>
