25ee76fb03b2680c4a4720cf04d291f2466756de
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-15 11:05:18 +0100

    Dropdown view with a custom event for selected option

---
+++ src/views/DropDownView.svelte
@@ -0,0 +1,24 @@
+<!-- SelectView.svelte -->
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	export let selectedOption: string;
+	export let options: { value: string; label: string }[];
+
+	const dispatch = createEventDispatcher();
+
+	function handleChange(event: Event): void {
+		const selectedValue = (event.target as HTMLSelectElement).value;
+		dispatch('change', selectedValue);
+	}
+</script>
+
+<select
+	class="rounded-full border border-gray-300 bg-white px-3 py-1 text-sm text-gray-700 focus:border-blue-500 focus:ring-blue-500"
+	bind:value={selectedOption}
+	on:change={handleChange}
+>
+	{#each options as { value, label }}
+		<option {value}>{label}</option>
+	{/each}
+</select>
