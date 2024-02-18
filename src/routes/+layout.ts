49c53b48b525d4ecc5cd5019e41f8896043cdfde
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-18 13:25:00 +0100

    Keep current locale state in URL

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,23 +1,26 @@
-import { addMessages, init } from 'svelte-i18n';
+import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
 import { locales } from '$lib/locales/localeConfig';
 
 /**
  * Load function for the root layout module.
  * Initializes the svelte-i18n library with locale messages imported from 'localeConfig.ts'.
- * Ensures internationalization is ready before rendering the application content.
+ * Sets the initial locale based on the 'lang' query parameter or defaults to the navigator language or 'en'.
  *
  * @async
  * @function load
+ * @param {Object} params - The load parameters provided by SvelteKit, including URL.
  * @returns {Promise<Object>} An empty object, as the load function must return an object.
  */
-export async function load(): Promise<object> {
+export async function load({ url }): Promise<object> {
 	Object.entries(locales).forEach(([locale, messages]) => {
 		addMessages(locale, messages);
 	});
 
+	const initialLocale = url.searchParams.get('lang') || getLocaleFromNavigator() || 'en';
+
 	await init({
 		fallbackLocale: 'en',
-		initialLocale: 'en'
+		initialLocale: initialLocale
 	});
 
 	return {};
