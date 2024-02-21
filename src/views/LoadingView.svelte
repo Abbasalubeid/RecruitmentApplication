6958d8b6f95a4b2602eca2b0633d98d9a72d75a5
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-21 21:04:17 +0100

    LoadingView style

--- src/views/LoadingView.svelte
+++ src/views/LoadingView.svelte
@@ -8,7 +8,7 @@
 
 <div class="flex flex-col items-center">
 	<div>
-		<ProgressRadial value={undefined} />
+		<ProgressRadial value={undefined} track="stroke-primary-500/30" />
 	</div>
 	<p class="mt-2">{displayedMessage}</p>
 </div>
