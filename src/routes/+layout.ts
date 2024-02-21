13f08baeb548513b9e4d3cb464cfd52a27340005
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-21 21:01:03 +0100

    Update userStore when refreshing

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,17 +1,21 @@
 import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
 import { locales } from '$lib/locales/localeConfig';
+import { userStore } from '$lib/stores/UserStore';
+import { Person } from '../models/Person';
 
 /**
- * Load function for the root layout module.
- * Initializes the svelte-i18n library with locale messages imported from 'localeConfig.ts'.
- * Sets the initial locale based on the 'lang' query parameter or defaults to the navigator language or 'en'.
+ * Load function for the root layout module. This function performs two primary tasks:
+ * 1. Initializes the svelte-i18n library with locale messages imported from 'localeConfig.ts'.
+ *    Sets the initial locale based on the 'lang' query parameter or defaults to the navigator language or 'en'.
+ * 2. Fetches the current user data from the '/api/auth/me' endpoint and updates the userStore with this data.
+ * This ensures that user data is always available in the store for access across different pages.
  *
  * @async
  * @function load
- * @param {Object} params - The load parameters provided by SvelteKit, including URL.
+ * @param {Object} params - The load parameters provided by SvelteKit, including fetch function and URL.
  * @returns {Promise<Object>} An empty object, as the load function must return an object.
  */
-export async function load({ url }): Promise<object> {
+export async function load({ url, fetch }): Promise<object> {
 	Object.entries(locales).forEach(([locale, messages]) => {
 		addMessages(locale, messages);
 	});
@@ -22,6 +26,20 @@ export async function load({ url }): Promise<object> {
 		fallbackLocale: 'en',
 		initialLocale: initialLocale
 	});
+
+	const response = await fetch('/api/auth/me');
+	if (response.ok) {
+		const userInfo = await response.json();
+		const user = new Person(
+			userInfo.person_id,
+			userInfo.name,
+			userInfo.surname,
+			userInfo.email,
+			userInfo.role,
+			userInfo.username
+		);
+		userStore.updateUser(user);
+	}
 
 	return {};
 }
