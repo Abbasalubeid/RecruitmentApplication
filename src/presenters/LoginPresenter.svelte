2e7499c6d791c5a70591358e3cfe8512a7ef802f
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-21 20:59:45 +0100

    Comments

--- src/presenters/LoginPresenter.svelte
+++ src/presenters/LoginPresenter.svelte
@@ -1,12 +1,26 @@
 <script lang="ts">
+	/**
+	 * The `LoginPresenter` serves as the intermediary between the login view and the backend authentication service.
+	 * It handles user login events, processes login requests, manages login state (loading, errors).
+	 */
+
 	import LoginView from '../views/LoginView.svelte';
 	import { t } from 'svelte-i18n';
 	import { goto } from '$app/navigation';
+	import { userStore } from '$lib/stores/UserStore';
 	import { Person } from '../models/Person';
 
 	let errorKey: string | undefined;
 	let loading = false;
 
+	/**
+	 * Handles the login event emitted by the `LoginView` component. It performs a POST request to the
+	 * '/api/auth/login' endpoint with the provided username and password. Upon successful authentication,
+	 * it updates the userStore with the logged-in user's information and redirects to the homepage.
+	 * In case of an error, it sets the appropriate error message to be displayed.
+	 *
+	 * @param {Object} event - The event object containing the username and password.
+	 */
 	const handleLogin = async (event: { detail: { username: string; password: string } }) => {
 		loading = true;
 		const { username, password } = event.detail;
@@ -28,7 +42,7 @@
 				return;
 			}
 
-			const userInfo = await res.json();
+			const { userInfo } = await res.json();
 			const person = new Person(
 				userInfo.person_id,
 				userInfo.name,
@@ -37,6 +51,7 @@
 				userInfo.role,
 				userInfo.username
 			);
+			userStore.updateUser(person);
 
 			const queryParams = new URLSearchParams(window.location.search);
 			const lang = queryParams.get('lang');
