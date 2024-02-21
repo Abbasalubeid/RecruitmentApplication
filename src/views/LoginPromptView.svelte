a98d80761748733ea32a5b0e4c848aa3394dd6ea
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-21 22:52:08 +0100

    Prevent on hover behavior

--- src/views/LoginPromptView.svelte
+++ src/views/LoginPromptView.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import { t } from 'svelte-i18n';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	function requestLogin() {
+		dispatch('requestLogin');
+	}
 </script>
 
 <div class="flex min-h-screen flex-col items-center justify-center space-y-4">
@@ -8,10 +15,10 @@
 	>
 		{$t('logInToContinue')}
 	</p>
-	<a
-		href="/login"
-		class="inline-block cursor-pointer rounded-full border-2 border-primary-500 px-6 py-3 hover:bg-primary-900"
+	<button
+		on:click={requestLogin}
+		class="inline-block cursor-pointer rounded-full border-2 border-primary-500 bg-transparent px-6 py-3"
 	>
 		{$t('login')}
-	</a>
+	</button>
 </div>
