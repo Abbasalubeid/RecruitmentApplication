3a7dcfae6a82fbbcc7b93a9acd97414686e1acbc
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-25 22:50:39 +0100

    Translate role

--- src/views/UserInfoView.svelte
+++ src/views/UserInfoView.svelte
@@ -3,8 +3,6 @@
 	export let fullName: string;
 	export let role: string | undefined;
 	export let username: string;
-
-	let currentRole = role ? role : '';
 </script>
 
 <div class="flex min-h-screen flex-col items-center justify-center space-y-4">
@@ -14,5 +12,7 @@
 		{$t('welcome')}, {fullName}!
 	</h1>
 	<h3 class="text-xl">{$t('username')}: {username}</h3>
-	<h3 class="text-xl">{$t('role')}: {$t(currentRole)}</h3>
+	{#if role}
+		<h3 class="text-xl">{$t('role')}: {$t(role)}</h3>
+	{/if}
 </div>
