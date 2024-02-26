6afcb7dd698d2aceaaff570e0fb6fa275ef96898
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-26 22:47:22 +0100

    Metadata about routes for protection

---
+++ src/lib/metadata/routeMeta.ts
@@ -0,0 +1,26 @@
+/**
+ * Defines metadata for application routes, specifying access control rules and behaviors.
+ * This configuration allows for granular control over who can access specific routes based
+ * on authentication status and user roles. It also specifies custom behaviors, such as
+ * redirecting authenticated users away from certain routes.
+ *
+ * - `requiresAuth`: Indicates that a route requires the user to be authenticated.
+ * - `roles`: An array of roles allowed to access the route. Used with `requiresAuth`.
+ * - `redirectIfAuthenticated`: Specifies a path to redirect authenticated users to, useful for routes like '/login'.
+ */
+
+interface RouteMeta {
+	requiresAuth: boolean;
+	redirectIfAuthenticated?: string;
+	roles?: string[];
+}
+
+interface RouteMetadata {
+	[key: string]: RouteMeta;
+}
+
+export const routeMetadata: RouteMetadata = {
+	'/': { requiresAuth: false },
+	'/login': { requiresAuth: false, redirectIfAuthenticated: '/' },
+	'/applications': { requiresAuth: true, roles: ['applicant'] }
+};
