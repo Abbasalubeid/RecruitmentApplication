1ab282a6fa34e5fb7a6cee5834073b51943b7aec
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-26 22:53:27 +0100

    Use theme colors and make errorStatus optional

--- src/views/ErrorView.svelte
+++ src/views/ErrorView.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+	import { navigateWithQuery } from '$lib/util/navigation';
+
 	export let errorMessage: string;
-	export let errorStatus: number = 500;
+	export let errorStatus: number | undefined = undefined;
 
 	import 'iconify-icon';
 	import { t } from 'svelte-i18n';
@@ -9,24 +11,25 @@
 {#if errorMessage}
 	<div class="flex h-screen items-center justify-center">
 		<div
-			class="alert variant-ghost flex w-full max-w-md items-center justify-between rounded-lg border border-red-400 bg-red-100 text-red-700"
+			class="alert variant-ghost flex w-full max-w-md items-center justify-between rounded-lg border border-error-400 bg-error-100 text-error-700"
 		>
-			<div class="alert-errorMessage">
-				<h3 class="text-lg font-semibold">
-					Error {#if typeof errorStatus === 'number'}
-						- {errorStatus}{/if}
-				</h3>
-				<p>{errorMessage}</p>
+			<iconify-icon icon="bi:exclamation-triangle-fill" class="text-error-500" width="35"
+			></iconify-icon>
+			<div>
+				<h5 class="h5">
+					{#if typeof errorStatus === 'number'}
+						Error - {errorStatus}{/if}
+				</h5>
+				<h5 class="h5">{errorMessage}</h5>
 			</div>
-			<a href="/">
-				<button
-					title="Go to homepage"
-					class="flex items-center gap-2 rounded-full bg-red-700 px-4 py-2 text-white hover:bg-red-800"
-				>
-					<iconify-icon icon="line-md:arrow-left" class="text-white" width="24" height="24" />
-					<span>{$t('goToHomepage')}</span>
-				</button>
-			</a>
+			<button
+				on:click={() => navigateWithQuery(`/`)}
+				title="Go to homepage"
+				class="flex items-center gap-2 rounded-full bg-error-700 px-4 py-2 hover:bg-error-800"
+			>
+				<iconify-icon icon="line-md:arrow-left" width="24" height="24" />
+				<span>{$t('goToHomepage')}</span>
+			</button>
 		</div>
 	</div>
 {/if}
