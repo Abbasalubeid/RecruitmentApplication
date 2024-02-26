d19971f4a2fae327f5ff2c28434c0fa9462dcd7e
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-02-26 15:04:31 +0100

    Added server side navigation function

--- src/lib/util/navigation.ts
+++ src/lib/util/navigation.ts
@@ -1,4 +1,5 @@
 import { goto } from '$app/navigation';
+import { redirect } from '@sveltejs/kit';
 
 /**
  * Navigates to a specified path while preserving the 'lang' query parameter from the current URL.
@@ -13,5 +14,30 @@ import { goto } from '$app/navigation';
 export function navigateWithQuery(path: string) {
 	const queryParams = new URLSearchParams(window.location.search);
 	const lang = queryParams.get('lang');
-	goto(`${path}?${lang ? `lang=${lang}` : ''}`);
+	const queryString = lang ? `lang=${lang}` : '';
+
+	const fullPath = queryString ? `${path}?${queryString}` : path;
+
+	goto(fullPath);
+}
+
+/**
+ * Redirects to a specified path on the server side while preserving the 'lang' query parameter.
+ *
+ * This function is designed for server-side usage where the `window` object is not available. It
+ * extracts the 'lang' parameter from the provided query string and appends it to the target path.
+ * If the 'lang' parameter is not present in the query string, it redirects to the path without any
+ * query parameters. This ensures that the language preference is maintained across redirects.
+ *
+ * @param {string} query - The query string from the current URL, used to extract the 'lang' parameter.
+ * @param {string} path - The target path to redirect to. This should be a relative path starting with '/'.
+ */
+export function redirectWithQuery(query: string, path: string) {
+	const queryParams = new URLSearchParams(query);
+	const lang = queryParams.get('lang');
+	const queryString = lang ? `lang=${lang}` : '';
+
+	const fullPath = queryString ? `${path}?${queryString}` : path;
+
+	redirect(303, fullPath);
 }
