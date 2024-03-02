de739a7c2afb7950e67c340d25e44f85d841745c
zubair-y <muhammad.yousafzai@hotmail.com>
2024-03-02 23:07:21 +0100

    Added validation for user input

--- src/presenters/ApplicationPresenter.svelte
+++ src/presenters/ApplicationPresenter.svelte
@@ -5,9 +5,10 @@
 	let expertise: Array<{ id: number; name: string }> = [];
 	let errorKey: string | undefined;
 	let errorStatus: number | undefined;
+	let errorMessage: string = '';
 
 	let addedExperiences: Array<{ expertise: string; years: number }> = [];
-	let addedAvailability: Array<{ startDay: number; endDay: number }> = [];
+	let addedAvailability: Array<{ startDay: string; endDay: string }> = [];
 
 	/**
 	 * Lifecycle hook for component initialization and data fetching. It attempts to fetch competence data from the API, and handles any errors that might occur during the fetch operation.
@@ -28,11 +29,56 @@
 	});
 
 	function onAddExperience(expertise: string, years: number) {
+		if (!expertise && !years) {
+			errorMessage = 'Please provide an area of expertise and a number of years of experience.';
+			return;
+		} else if (!expertise) {
+			errorMessage = 'Please provide an area of expertise.';
+			return;
+		} else if (!years) {
+			errorMessage = 'Please provide a number of years of experience.';
+			return;
+		}
+
+		if (years > 99) {
+			errorMessage = 'Please provide a number of years of experience less than 100.';
+			return;
+		} else if (years < 0) {
+			errorMessage = 'Please provide a number of years of experience greater than 0.';
+			return;
+		}
+
+		if (addedExperiences.some((exp) => exp.expertise === expertise)) {
+			errorMessage = 'You have already added this experience.';
+			return;
+		}
+
 		addedExperiences = [...addedExperiences, { expertise, years }];
+		errorMessage = '';
 	}
 
-	function onAddAvailability(startDay: number, endDay: number) {
+	function onAddAvailability(startDay: string, endDay: string) {
+		if (!startDay && !endDay) {
+			errorMessage = 'Please provide both start day and end day.';
+			return;
+		} else if (!startDay) {
+			errorMessage = 'Please provide a start day.';
+			return;
+		} else if (!endDay) {
+			errorMessage = 'Please provide an end day.';
+			return;
+		}
+
+		if (new Date(startDay) < new Date()) {
+			errorMessage = 'Start day cannot be in the past.';
+			return;
+		} else if (new Date(endDay) < new Date(startDay)) {
+			errorMessage = 'End day cannot be before the start day.';
+			return;
+		}
+
 		addedAvailability = [...addedAvailability, { startDay, endDay }];
+		errorMessage = '';
 	}
 </script>
 
@@ -42,4 +88,5 @@
 	{onAddAvailability}
 	{addedExperiences}
 	{addedAvailability}
+	{errorMessage}
 />
