1d4c78a865e2ddcd0158e65e002811999d759cf9
Otto Heile <ottoheilee@gmail.com>
2024-03-04 23:49:58 +0100

    fixed supplying missing person data only working for applicants; now also works for recruiters

--- src/presenters/MigrationPresenter.svelte
+++ src/presenters/MigrationPresenter.svelte
@@ -1,22 +1,53 @@
 <script lang="ts">
 	import { t } from 'svelte-i18n';
 	import FormView from '../views/FormView.svelte';
+	import LoadingView from '../views/LoadingView.svelte';
 	import StatusView from '../views/StatusView.svelte';
+	import Validator from '$lib/util/validator';
+	import { onMount } from 'svelte';
 
 	export let token: string | null;
 
 	let errorKey: string | undefined;
-	let loading = false;
+	let loading = true;
 	let success = false;
 	let validToken = true;
+	let roleID = -1;
 
-	// TODO
-	// Set based on role_id from token in URL
+	let inputErrors: Record<string, string | undefined> = {};
+
 	$: title = $t('setupAccount');
-	$: inputs = [
+	$: recruiterInputs = [
+		{ name: 'pnr', label: $t('pnr'), placeholder: $t('pnrFormat'), type: 'text' },
+		{ name: 'email', label: $t('email'), placeholder: $t('enterEmail'), type: 'text' },
+	];
+
+	$: applicantInputs = [
 		{ name: 'username', label: $t('username'), placeholder: $t('chooseUsername'), type: 'text' },
 		{ name: 'password', label: $t('password'), placeholder: $t('choosePassword'), type: 'password' }
 	];
+	
+	onMount(async () => {
+		const response = await fetch('/api/auth/migration/role_id', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({token})
+		});
+
+		if (!response.ok) {
+			validToken = false;
+			loading = false;
+			return;
+		}
+
+		const result = await response.json();
+
+		roleID = result.role_id;
+
+		loading = false;
+	});
 
 	/**
 	 * Handle the submit event emitted by the `FormView` component. A POST request to the '/api/auth/migration' endpoint with
@@ -37,6 +68,14 @@
 	}) {
 		loading = true;
 		const formData = event.detail.formData;
+
+		inputErrors = Validator.validateForm(formData);
+
+		// If any validation errors exist, do not proceed
+		if (Object.keys(inputErrors).some((key) => inputErrors[key] !== undefined)) {
+			loading = false;
+			return;
+		}
 
 		try {
 			const response = await fetch('/api/auth/migration', {
@@ -62,21 +101,34 @@
 	};
 
 	$: errorMessage = errorKey ? $t(errorKey) : undefined;
+
+	$: translatedInputErrors = Object.entries(inputErrors).reduce(
+		(acc: Record<string, string | undefined>, [key, errorKey]) => {
+			acc[key] = errorKey ? $t(errorKey) : undefined;
+			return acc;
+		},
+		{}
+	);
 </script>
 
-{#if validToken}
-	{#if success}
-		<StatusView message={$t('setupSuccess')} viewType="welcome" />
-	{:else}
-		<FormView
-			on:submit={handleMissingData}
-			{inputs}
-			buttonText={$t('submit')}
-			title={title}
-			{errorMessage}
-			{loading}
-		/>
-	{/if}
+{#if loading}
+	<LoadingView />
 {:else}
-	<StatusView viewType='error' statusNumber={404} message={$t('error.pageNotFound')} />
+	{#if validToken}
+		{#if success}
+			<StatusView message={$t('setupSuccess')} viewType="welcome" />
+		{:else}
+			<FormView
+				on:submit={handleMissingData}
+				inputs={roleID == 1 ? recruiterInputs : applicantInputs}
+				buttonText={$t('submit')}
+				title={title}
+				{errorMessage}
+				inputErrors={translatedInputErrors}
+				{loading}
+			/>
+		{/if}
+	{:else}
+		<StatusView viewType='error' statusNumber={404} message={$t('error.pageNotFound')} />
+	{/if}
 {/if}
