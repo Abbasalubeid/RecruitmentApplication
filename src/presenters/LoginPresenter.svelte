ae90a2fa9a665d5feef2bc42478076a862392323
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-03-05 20:36:26 +0100

    Format

--- src/presenters/LoginPresenter.svelte
+++ src/presenters/LoginPresenter.svelte
@@ -36,7 +36,6 @@
 		inputErrors = Validator.validateForm(formData);
 
 		// If any validation errors exist, do not proceed
-
 		if (Object.keys(inputErrors).some((key) => inputErrors[key] !== undefined)) {
 			loading = false;
 			return;
