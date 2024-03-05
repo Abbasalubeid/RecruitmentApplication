bdcf0c92a15b6a888dd20818ec220a5567d84a93
Abbas Alubeid <abbasalubeid1@gmail.com>
2024-03-05 23:10:13 +0100

    Position styling for UserInfoView, FormView and LoginPromptView

--- src/views/LoginPromptView.svelte
+++ src/views/LoginPromptView.svelte
@@ -3,7 +3,7 @@
 	import { navigateWithQuery } from '$lib/util/navigation';
 </script>
 
-<div class="flex min-h-screen flex-col items-center justify-center space-y-4">
+<div class="mt-[20vh] flex flex-grow flex-col items-center justify-end space-y-4">
 	<p
 		class="bg-gradient-to-br from-primary-400 to-primary-800 bg-clip-text text-center text-2xl font-bold text-transparent"
 	>
